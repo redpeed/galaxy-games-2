a8f3a79fc8e54516a49094cab0f62659714ec91f
mattbreda <mattbreda@gmail.com>
2025-05-27 18:27:06 +0200

    Feature: new page with staking platform included via iframe

---
+++ src/app/staking/page.tsx
@@ -0,0 +1,14 @@
+function StakingPlatform() {
+  return (
+    <div className="miw-w-screen min-h-screen h-[100dvh] w-[100dvw] bg-[#01011e]">
+      <iframe
+        src="https://galaxy-staging.netlify.app/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  );
+}
+
+export default StakingPlatform;
